65e8e2369d987ba524ecd186ba284e41a140d3f4
LuoMo92 <810754420@qq.com>
2017-10-13 10:14:28 +0800

    统一异常处理

--- src/main/java/com/luomo/study/spring/boot/chapter1/web/HelloController.java
+++ src/main/java/com/luomo/study/spring/boot/chapter1/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.luomo.study.spring.boot.chapter1.web;
 
+import com.luomo.study.spring.boot.exception.MyException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index(){
-        return "Hello World!";
+    public String index() throws Exception{
+        throw new Exception("发生错误");
     }
 
+    @RequestMapping("json")
+    public String json() throws MyException{
+        throw new MyException("发生error");
+    }
 }
---
+++ src/main/java/com/luomo/study/spring/boot/exception/MyException.java
@@ -0,0 +1,12 @@
+package com.luomo.study.spring.boot.exception;
+
+/**
+ * Created by LiuMei on 2017-10-13.
+ */
+public class MyException extends Exception {
+
+    public MyException(String message){
+        super(message);
+    }
+
+}
